f5bbfe0506dbc02187971c53992afb5734c5b291
Vladimir Knyazev <darknyaz@yandex.ru>
2020-09-19 14:42:40 +0300

    Simple authentication

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
+from src.db import db
 app = Flask(__name__)
+DB = db.DbConnection()
 
 @app.route('/')
 def hello_world():
-    username = request.args.get('user')
+    login = request.cookies.get('login')
+    username = ''
+
+    if login:
+        names_logins = DB.execute_select_query('SELECT name, login FROM User')
+        user_rec = next(filter(lambda x: x[1] == login, names_logins))
+        if user_rec:
+            username = user_rec[0]
+
     return render_template('index.html', username=username)
+
+@app.route('/login', methods=['POST'])
+def login_handler():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not all([login, password]):
+        return '', 500
+
+    user_table_recs = DB.execute_select_query(
+        'SELECT name, login, password FROM User')
+
+    user_rec = next(filter(lambda x: x[1] == login, user_table_recs))
+
+    if not user_rec:
+        return '', 500
+
+    if user_rec[2] != password:
+        return '', 500
+
+    response = make_response(redirect('/'))
+    response.set_cookie('login', user_rec[1])
+    return response
---
+++ src/db/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+class DbConnection:
+    def __init__(self):
+        self.connection = sqlite3.connect('blog-2020.db',
+                                          check_same_thread=False)
+        self.select_cursor = self.connection.cursor()
+
+    def execute_select_query(self, query):
+        return self.select_cursor.execute(query).fetchall()
